9a2f5b1ebb941979bc5ae64704ec303dbe193c5a
John Staudt <johnstaudt.sa@gmail.com>
2020-01-08 20:32:24 -0600

    Add postgres, remove uuid, and begin refactor for persistence layer.

--- src/tasks/pipes/task-status-validation.pipes.ts
+++ src/tasks/pipes/task-status-validation.pipes.ts
@@ -1,6 +1,6 @@
 // tslint:disable:no-console
 import { PipeTransform, BadGatewayException } from '@nestjs/common';
-import { TaskStatus } from '../tasks.model';
+import { TaskStatus } from '../task-status.enum';
 
 export class TaskStatusValidationPipe implements PipeTransform {
     readonly allowedStatuses = [
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,35 +1,39 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Task, TaskStatus } from './tasks.model';
-import * as uuid from 'uuid/v1';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskRepository } from './task.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Task } from './task.entity';
+import { TaskStatus } from './task-status.enum';
 
 @Injectable()
 export class TasksService {
-    private tasks: Task[] = [];
+    constructor(
+        @InjectRepository(TaskRepository)
+        private taskRepository: TaskRepository,
+    ) {}
+    // getAllTasks(): Task[] {
+    //     return this.tasks;
+    // }
 
-    getAllTasks(): Task[] {
-        return this.tasks;
-    }
+    // getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+    //     const { status, search  } = filterDto;
 
-    getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
-        const { status, search  } = filterDto;
+    //     let tasks = this.getAllTasks();
 
-        let tasks = this.getAllTasks();
+    //     if (status) {
+    //         tasks = tasks.filter(task => task.status === status);
+    //     }
 
-        if (status) {
-            tasks = tasks.filter(task => task.status === status);
-        }
+    //     if (search) {
+    //         tasks = tasks.filter(task => task.title.includes(search) || task.description.includes(search));
+    //     }
 
-        if (search) {
-            tasks = tasks.filter(task => task.title.includes(search) || task.description.includes(search));
-        }
+    //     return tasks;
+    // }
 
-        return tasks;
-    }
-
-    getTaskById(id: string): Task {
-        const found = this.tasks.find(task => task.id === id);
+    async getTaskById(id: number): Promise<Task> {
+        const found = await this.taskRepository.findOne(id);
 
         if (!found) {
             throw new NotFoundException(`Task with ${id} not found`);
@@ -38,31 +42,35 @@ export class TasksService {
         return found;
     }
 
-    deleteTask(id: string): void {
-        const found = this.getTaskById(id);
+    // deleteTask(id: string): void {
+    //     const found = this.getTaskById(id);
 
-        this.tasks = this.tasks.filter(task => task.id !== found.id);
+    //     this.tasks = this.tasks.filter(task => task.id !== found.id);
+    // }
+
+    // updateTaskStatus(id: string, status: TaskStatus): Task {
+    //     const task = this.getTaskById(id);
+    //     task.status = status;
+
+    //     return task;
+    // }
+
+    async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+        return this.taskRepository.createTask(createTaskDto);
     }
 
-    updateTaskStatus(id: string, status: TaskStatus): Task {
-        const task = this.getTaskById(id);
-        task.status = status;
+    // createTask(createTaskDto: CreateTaskDto): Task {
+    //     const { title, description } = createTaskDto;
 
-        return task;
-    }
+    //     const task: Task = {
+    //         id: uuid(),
+    //         title,
+    //         description,
+    //         status: TaskStatus.OPEN,
+    //     };
 
-    createTask(createTaskDto: CreateTaskDto): Task {
-        const { title, description } = createTaskDto;
+    //     this.tasks.push(task);
 
-        const task: Task = {
-            id: uuid(),
-            title,
-            description,
-            status: TaskStatus.OPEN,
-        };
-
-        this.tasks.push(task);
-
-        return task;
-    }
+    //     return task;
+    // }
 }
